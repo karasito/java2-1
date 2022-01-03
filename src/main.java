bc570fffdffdb0e4acbb3a030cda902834790694
karasito <94555947+karasito@users.noreply.github.com>
2022-01-03 18:05:38 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,21 +1,19 @@
 public class main {
-    public static void main (String[] args) {
-        int UserAccount = 100 ; // Счет пользователя
-        int  ReceiptOofMoney = 1500 ; // Поступление денег
-        int fullBonusAmount = 100 ; // Полная сумма бонуса
-        int numberOfbonuses = 1 ; // Количество бонусов
-        int check = UserAccount + ReceiptOofMoney ; //Проверка поступлений на счет
-        boolean bonus = ReceiptOofMoney > 1000 ;
-        if (ReceiptOofMoney > 1000)  {
-            int FinalBonus = UserAccount + ReceiptOofMoney + (ReceiptOofMoney / 100) * numberOfbonuses    ;
-            System.out.println("Счет с бонусами: " + FinalBonus ) ;
+    public static void main(String[] args) {
+        int UserAccount = 100; // Счет пользователя
+        int ReceiptOofMoney = 1500; // Поступление денег
+        int fullBonusAmount = 100; // Полная сумма бонуса
+        int numberOfbonuses = 1; // Количество бонусов
+        int check = UserAccount + ReceiptOofMoney; //Проверка поступлений на счет
+        boolean bonus = ReceiptOofMoney > 1000;
+        if (ReceiptOofMoney > 1000) {
+            int FinalBonus = UserAccount + ReceiptOofMoney + (ReceiptOofMoney / 100) * numberOfbonuses;
+            System.out.println("Счет с бонусами: " + FinalBonus);
 
-        }
-        else {
-            int FinalBonus = UserAccount + ReceiptOofMoney   ;
-            System.out.println("Счет без бонусов: " + FinalBonus) ;
+        } else {
+            int FinalBonus = UserAccount + ReceiptOofMoney;
+            System.out.println("Счет без бонусов: " + FinalBonus);
         }
 
 
     }
-}
